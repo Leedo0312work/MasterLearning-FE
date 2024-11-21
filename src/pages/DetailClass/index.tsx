3524e0cd89501a3d046f0515e4430c8799d499b3
manh1cuong23 <vomanhcuong06102003@gmail.com>
2024-11-21 18:28:23 +0700

    abc

--- src/pages/DetailClass/index.tsx
+++ src/pages/DetailClass/index.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Outlet } from 'react-router-dom';
 import SidebarClass from '~/components/SidebarClass';
 
@@ -14,24 +13,6 @@ function DetailClass() {
             </div>
         </div>
     );
-=======
-import { Outlet } from "react-router-dom";
-import SidebarClass from "~/components/SidebarClass";
-import SidebarClassAdmin from "~/components/SidebarClassAdmin";
-
-function DetailClass() {
-  return (
-    <div>
-      <div>
-
-        <SidebarClass />
-      </div>
-      <div className={"tw-ml-[240px]"}>
-        <Outlet />
-      </div>
-    </div>
-  );
->>>>>>> 7ded045da28b53dfd0d14877646abca0bcb8cca2
 }
 
 export default DetailClass;
